08f446df725519a1042cfcb27c66bceca47a1787
tankche1 <15307130191@fudan.edu.cn>
2017-08-24 15:26:12 +0800

    origin PPO version

---
+++ test.py
@@ -0,0 +1,17 @@
+from osim.env import RunEnv
+
+env = RunEnv(visualize=False)
+observation = env.reset(difficulty = 0)
+for i in range(200):
+    observation, reward, done, info = env.step(env.action_space.sample())
+    print(type(observation))
+    print(len(observation))
+    print(type(reward))
+    print(reward)
+    print(type(done))
+    print(done)
+    print(type(info))
+    print(info)
+    print(type(env.action_space.sample()))
+    print(env.action_space.sample())
+    print('-----------------------')
