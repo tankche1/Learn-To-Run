11496308c7647e200503770f8f86908512dd3178
tankche1 <15307130191@fudan.edu.cn>
2017-08-24 15:30:31 +0800

    add PPO

---
+++ PPO/test.py
@@ -0,0 +1,18 @@
+from osim.env import RunEnv
+import scipy
+
+env = RunEnv(visualize=False)
+observation = env.reset(difficulty = 0)
+for i in range(200):
+    observation, reward, done, info = env.step(env.action_space.sample())
+    print(type(observation))
+    print(len(observation))
+    print(type(reward))
+    print(reward)
+    print(type(done))
+    print(done)
+    print(type(info))
+    print(info)
+    print(type(env.action_space.sample()))
+    print(env.action_space.sample())
+    print('-----------------------')
